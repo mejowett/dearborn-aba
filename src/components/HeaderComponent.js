a2ae88886313e4e75918eaf127b3ec2671db5c94
mejowett <78455126+mejowett@users.noreply.github.com>
2021-06-28 15:32:40 -0400

    first commit

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+Import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+
+class HeaderComponent extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default HeaderComponent;
